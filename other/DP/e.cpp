e7eb707326aac36dc71c1d172ce7e856146363a5
松井　祐希 <yuhki@GloogaDualiesDeco.local>
2019-04-15 10:08:11 +0900

    DP e

--- other/DP/e.cpp
+++ other/DP/e.cpp
@@ -26,43 +26,42 @@ int W;
 long long w[110];
 long long v[110];
 int f;
+const long long INF = 1LL << 60;
 
-// DP テーブル
-//long long dp[110][100010];
 
 int main() {
     cin >> N >> W;
     for (int i = 0; i < N; ++i) cin >> w[i] >> v[i];
     
-    int wmax = 10;
+    int wmax = 0;
     int vmax = 0;
     for (int i = 0; i < N; ++i){
       wmax = wmax + w[i];
       vmax = vmax + v[i];
     }
 
-    long long dp[110][vmax];
 
-    for (int i= 0; i < N;++i){
-      for (int j=0; j <vmax ; ++ j) dp[i][j] = wmax + 100;  
+    long long dp[110][vmax];
+    for (int i=0; i < N + 1;++i){
+      for (int j=0; j <vmax + 1 ; ++ j) dp[i][j] = INF;
     }
 
-    cout << dp[1][1] << endl;
+    dp[0][0] = 0; 
 
     // ループ
     for (int i = 0; i < N; ++i) {
-      for (int j = 0; j <= vmax; ++j){
+      for (int j = 0; j < vmax + 1; ++j){
         if(j - v[i] >= 0){
           chmin(dp[i+1][j],dp[i][j-v[i]] + w[i]);
           }
         chmin(dp[i+1][j],dp[i][j]);
-        
+                
       }
     }
 
     // 最適値の出力
     long long res = 0;
-    for (int sum_v = 0; sum_v < vmax; ++sum_v) {
+    for (int sum_v = 0; sum_v < vmax + 1; ++sum_v) {
         if (dp[N][sum_v] <= W) res = sum_v;
     }
     cout << res << endl;
